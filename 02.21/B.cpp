ac1f4eac1ea05ffc450ee912fab3c3d90822feaa
look-in-a <look-in-a@mail.ru>
2018-02-22 17:56:00 +0300

    02.21 complete

--- 02.21/B.cpp
+++ 02.21/B.cpp
@@ -1,30 +1,68 @@
-// 21.02.2018  21-27
+// 21.02.2018 
 // http://codeforces.com/contest/916
 
-
 #include <iostream>
+#include <algorithm>
+#include <map>
 
 using namespace std;
 
-bool isLucky(int time) {
-	int min = time % 60;
-	int hour = time / 60;
-	return (min % 10 == 7 || hour % 10 == 7);
-}
-
 int main() {
-	int x, h, m;
-	cin >> x >> h >> m;
+	map<long long,long long> degrees;
+	long long n, k, r, deg, max;
+	cin >> n >> k;
 
-	int time = h * 60 + m;
-	int num = 0;
-	while (!isLucky(time)) {
-		time-=x;
-		if (time < 0) {
-			time += 24 * 60;
+	r = 0;
+	deg = 0;
+	while (n > 0) {
+		if (n % 2 == 1) {
+			degrees[deg] = 1;
+			max = deg;
+			r++;
 		}
-		num++;
+		n /= 2;
+		deg++;
 	}
-	cout << num << "\n";
+
+	if (r > k) {
+		cout << "No" << "\n";
+		return 0;
+	}
+	cout << "Yes" << "\n";
+
+	while (degrees[max] <= k-r){
+		degrees[max - 1] += 2 * degrees[max];
+		r += degrees[max];
+		degrees.erase(max);
+		max--;
+	}
+
+	vector<long long> degreesVec;
+	for (auto i : degrees) {
+		degreesVec.push_back(i.first);
+	} 
+	sort(degreesVec.begin(), degreesVec.end());
+	int min = degreesVec[0];
+	
+	while (r < k) {
+		degrees[min - 1] += 2;
+		degrees[min] -= 1;
+		min--;
+		r++;
+	}
+
+	vector<long long> degreesVec2;
+	for (auto i : degrees) {
+		degreesVec2.push_back(i.first);
+	}
+
+	long  long size = degreesVec2.size();
+	for (long long i  = size - 1; i >= 0; i--) {
+		for (long long j = 0; j < degrees[degreesVec2[i]]; j++) {
+			cout << degreesVec2[i] << " ";
+		}
+	}
+
+	cout << "\n";
 	return 0;
 }
