1a1e0b0b0f0d2d85d5ca85224a953ff6ee67237c
look-in-a <look-in-a@mail.ru>
2018-03-01 14:19:40 +0300

    #452

--- 03.01/A.cpp
+++ 03.01/A.cpp
@@ -1,5 +1,5 @@
 // 01.03.2018
-//
+// http://codeforces.com/contest/899
 
 
 #include <iostream>
@@ -8,5 +8,23 @@ using namespace std;
 
 int main() {
 
+	int n;
+	cin >> n;
+	int couples = 0;
+	int singles = 0;
+	for (int i = 0; i < n; i++) {
+		int c;
+		cin >> c;
+		if (c == 2) {
+			couples++;
+		} else {
+			singles++;
+		}
+	}
+	int commands = min(couples, singles);
+	couples -= commands;
+	singles -= commands;
+	commands += singles / 3;
+	cout << commands << "\n";
 	return 0;
 }
