b0439e604ece5af2c62ebbdb6ffbb38b327332a0
look-in-a <look-in-a@mail.ru>
2018-03-08 22:36:07 +0300

    #448

---
+++ 03.07/B.cpp
@@ -0,0 +1,36 @@
+// 07.03.2018
+// http://codeforces.com/contest/895
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+int cnt;
+
+int main () {
+	int n, x, k;
+	cin >> n >> x >> k;
+	map<int,int> a;
+	map<int, int> a1;
+	for (int i = 0; i < n; ++i ) {
+		int b;
+		cin >> b;
+		++a[b];
+	}
+	long long cnt = 0;
+	//sort(a.begin(), a.end());
+	for (auto i = a.rbegin(); i != a.rend(); ++i) {
+		if (i->first % x != 0 || k != 0) {
+			a1[i->first / x] += i->second;
+			//cout << a1[a[i]/x] << " ";
+		}
+		int b = k + (i->first - 1)/x;
+		long long second = i->second;
+		cnt += second * a1[b];
+	}
+	cout << cnt << "\n";
+	return 0;
+}
