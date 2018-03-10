40d231cf84ef8fad894b4a68a7cc52a2517d35b8
look-in-a <look-in-a@mail.ru>
2018-03-10 17:41:19 +0300

    #446

--- 03.10/B.cpp
+++ 03.10/B.cpp
@@ -11,10 +11,8 @@ using namespace std;
 int main() {
 	long long n;
 	cin >>  n;
-	//vector<long long> a(n);
 	vector<long long> b(n, -1);
-	//auto begin = chrono::high_resolution_clock::now();
-
+	
 	for (long long i = 0; i < n; ++i) {
 		long long a;
 		cin >> a;
@@ -28,9 +26,6 @@ int main() {
 			alive++;
 		}
 	}
-	//auto end = chrono::high_resolution_clock::now();
-    
-	//cout<<chrono::duration_cast<chrono::milliseconds>(end-begin).count()<<"ms"<< endl;
 	cout << alive << "\n";
 	return 0;
 }
